6e36fd98e7c5727983dba24bb3f2a13b4089bbab
haseeb124 <haseebz124@gmail.com>
2023-09-27 19:38:58 +0500

    11-redux-toolkit-todo

---
+++ reduxToolkitTodo/src/App.jsx
@@ -0,0 +1,19 @@
+
+import AddTodo from './components/AddTodo'
+import Todo from './components/Todo'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>React redux Toolkit</h1>
+      <AddTodo />
+      <Todo />
+    </>
+  )
+}
+
+export default App
