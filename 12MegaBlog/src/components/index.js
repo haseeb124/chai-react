276af229db667c1aa8eaf7b0c8484686cc6dac7a
haseeb124 <haseebz124@gmail.com>
2023-10-01 05:14:30 +0500

    mega project in process

---
+++ 12MegaBlog/src/components/index.js
@@ -0,0 +1,8 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import  Logo  from "./Logo";
+import  Container  from "react-dom";
+import LogoutBtn from "./Header/LogoutBtn";
+
+
+export {Header, Footer, Logo, LogoutBtn, Container };
