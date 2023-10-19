6a6778f803bec769695f5086a30d38740ba75d7f
haseeb124 <haseebz124@gmail.com>
2023-10-19 05:03:07 +0500

    till post-form

--- 12MegaBlog/src/components/Input.jsx
+++ 12MegaBlog/src/components/Input.jsx
@@ -1,27 +1,35 @@
-import React, { useId} from 'react'
+import React, {useId} from 'react'
 
-const Input = React.forwardRef( function Input({
+const Input = ({
   label,
-  type = "text",
+  type= "text",
   className = "",
   ...props
-}, ref) {
 
-  const id = useId();
+
+
+}, ref) => {
+
+  const id = useId()
+
   return (
-    <div>
-      {label && <label className='inline-block mb-1 pl-1' htmlFor={id}>
+    <div className=' w-full'>
+      {label && <label htmlFor={id} className='inline-block mb-1 pl-1'  >
         {label}
       </label>}
+
       <Input 
-        type = {type}
-        className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
+        type={type}
+        className={`
+        px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full
+        ${className}`}
+        ref={ref}
         {...props}
         id={id}
-        ref={ref}
       />
     </div>
   )
-})
+}
 
-export default Input
+export default React.forwardRef(Input);
+
--- 12MegaBlog/src/components/index.js
+++ 12MegaBlog/src/components/index.js
@@ -3,5 +3,10 @@ import Footer from './Footer/Footer'
 import Logo from './Logo'
 import Container from './container/Container'
 import LogoutBtn from '../components/Header/LogoutBtn'
+import Button from '../components/Button'
+import Input from '../components/Input'
+import Login from '../components/Login'
+import RTE from '../components/RTE'
+import Select from '../components/Select'
 
-export {Header, Footer, Logo, Container, LogoutBtn} 
+export {Header, Footer, Logo, Container, LogoutBtn, Button, Login, Input, RTE, Select} 
